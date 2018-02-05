7922de4a537dedb054cee8305f32c57fed8a6198
Global User <staff@epicodus.com>
2018-02-05 11:07:37 -0800

    completed solution with frontend

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,45 @@
-var inputs = [2,5,9];
-
-
+//this branch assumes a student will offer inputs as separate variables instead of an array
 function runElevator(inputs){
-  var j = 0;
-  for (var i = 1; i <= inputs[2]; i++)
-  {
-    if (inputs.includes(i)){
-      console.log("go through inputs code");
-      console.log("Stopping on floor" + inputs[j]);
-      j++;
-    }
-    else {
-    console.log("Passing floor" + i);
-    }
-  }
-}
 
-runElevator(inputs);
+  inputs.sort();
+  var resultsArray = [];
+  var j = 0;
+
+  if (inputs[0] == 0 || inputs.includes("") || inputs.includes(NaN)) {
+    resultsArray.push("invalid number entered - please try again");
+  }
+  else {
+    for (var i = 1; i <= inputs[inputs.length-1]; i++) //when array is sorted, last index will always be highest number.
+    {
+      if (inputs.includes(i)){
+        resultsArray.push("Door open on floor: " + inputs[j]);
+        j++;
+      }
+      else {
+        resultsArray.push("Passing floor: " + i);
+      }
+    }
+    resultsArray.push("Returning to lobby.");
+    }
+  return resultsArray;
+}
+$(document).ready(function(){
+  $("#floorsToStop").submit(function(){
+    $("#resultsList").text("");
+    event.preventDefault();
+
+    var firstFloorToStop = parseInt($("#firstFloor").val());
+    var secondFloorToStop = parseInt($("#secondFloor").val());
+    var thirdFloorToStop = parseInt($("#thirdFloor").val());
+    var inputs = [firstFloorToStop,secondFloorToStop,thirdFloorToStop];
+    var resultsArray = runElevator();
+    resultsArray.forEach(function(result){
+      $("#resultsList").append("<li>" + result + "</li>");
+    });
+  });
+});
+
+
+//account for 0? No - elevators don't do that.
+//account for negative numbers? No.
+//account for submitting only two floors - this breaks it.
